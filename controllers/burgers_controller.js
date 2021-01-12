bdcb054489381046c789c6babefb59ca83eb6155
Romina Aguado <rominaaguado@Rominas-MacBook-Pro.local>
2021-01-11 21:42:16 -0600

    devore it bottom working in back end but not front end

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,11 +30,16 @@ router.put("/burgers/:id", function (req, res) {
 
   burger.update(
     {
-      devoured: true,
+      devoured: req.body.devoured,
     },
     condition,
-    function (data) {
-      res.redirect("/");
+    function (result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     }
   );
 });
